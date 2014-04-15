9616d7921906bbcad0e104076db4dab2055aa3d8
Kenneth Dahlstrøm <kenneth@keda.no>
2014-04-15 15:32:23 +0200

    added layout classes
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_toolbar.js
+++ app/assets/js/components/_component_toolbar.js
@@ -2,6 +2,6 @@
 
 
 Hiof.Toolbar = function() {
-  $("#main header").first().append('<menu id="toolbar" type="toolbar"></menu>');
+  $("#main header").first().append('<menu id="toolbar" class="lo-full" type="toolbar"></menu>');
 };
 
