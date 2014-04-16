503134203ab293432a332f93188cee2836c1a048
Kenneth Dahlstrøm <kenneth@keda.no>
2014-04-16 11:53:51 +0200

    Fixed a bug in getSvgIcon() file reference
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_getSvgIcon.js
+++ app/assets/js/components/_component_getSvgIcon.js
@@ -5,11 +5,9 @@ var Hiof = Hiof || {};
 Hiof.getSvgIcon = function(icon){
 
 
-
-  var url = "/assets/images/icons/" + icon + ".svg";
+  var url = "assets/images/icons/" + icon + ".svg";
 
   xhr = new XMLHttpRequest();
-
 
   xhr.open("GET",url,false);
   xhr.overrideMimeType("image/svg+xml");
