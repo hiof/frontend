d56c4220084bad39ee82f0e615b62580218c36c2
Kenneth Dahlstrøm <kenneth@keda.no>
2014-04-24 08:57:31 +0200

    Added quotes rotation functionality
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

---
+++ app/assets/js/components/_component_quotes.js
@@ -0,0 +1,19 @@
+Hiof.Quotes = function(quoteId) {
+
+  $.getJSON("assets/js/data/quotes.json", function(data) {
+    var randomEntry = data.quotes[Math.floor(Math.random()*data.quotes.length)];
+
+    if (randomEntry.id == quoteId){
+      //console.log("Random entry was equal");
+      randomEntry.id--;
+      if (randomEntry.id == "0"){
+        //console.log("Random entry turned zero");
+        randomEntry.id = "3";
+        //console.log(".. But is now: " + randomEntry.id);
+      }
+    }
+    var quote = '<blockquote class="cover-quote" data-id="' + randomEntry.id + '""><p>' + randomEntry.text + '</p><footer><cite>- ' + randomEntry.cite + '</cite></footer></blockquote>';
+    $('#content').append(quote);
+    $('.cover-quote').fadeIn("slow");
+  });
+};
