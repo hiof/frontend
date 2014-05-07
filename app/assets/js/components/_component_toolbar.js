5b7a8df639158aa42aa0973864ba272389edead0
Kenneth Dahlstrøm <kenneth@keda.no>
2014-05-07 15:02:34 +0200

    Appending the toolbar to the bottom of #main as a footer instead of to the #main header.first()
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_toolbar.js
+++ app/assets/js/components/_component_toolbar.js
@@ -5,7 +5,7 @@ Hiof.Toolbar = function() {
   if($("#index").length){
 
   }else{
-    $("#main header").first().append('<menu id="toolbar" class="lo-full" type="toolbar"></menu>');
+    $("#main").append('<footer class="lo-full"><header class="lo-full"><h3>Del eller skriv ut denne siden</h3></header><menu id="toolbar" class="lo-full" type="toolbar"></menu></footer>');
   }
 };
 
