e856a27a7f18328ef6071e56d192037a58946248
Kenneth Dahlstrøm <kenneth@keda.no>
2014-05-08 12:52:50 +0200

    Updated dist
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

---
+++ config/assets.config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'hiof' => array(
+        'assets' => array(
+            'css' => array(
+                '/css/theme-standard.bd2dbef7.min.css',
+            ),
+            'js' => array(
+                '/js/application.65b8bead.min.js',
+            ),
+        ),
+    )
+);
