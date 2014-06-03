fe1a8993dd322a1a5dba890ed40e0a3337145489
Kenneth Dahlstrøm <kenneth@keda.no>
2014-06-03 10:31:04 +0200

    - Removed a console.log
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_contacts.js
+++ app/assets/js/components/_component_contacts.js
@@ -3,7 +3,7 @@ var Hiof = Hiof || {};
 
 
 Hiof.ChangeContactSearch = function(value){
-  console.log(value);
+  //console.log(value);
   $("div[id^='functionbuilder']").hide();
   $(value).slideDown();
 
