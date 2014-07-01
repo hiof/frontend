0ae94041e2c1efbca5b4d0af469c96ac06bd49c9
Kenneth Dahlstrøm <kenneth@keda.no>
2014-07-01 14:29:38 +0200

    - Added breakpoint for the content-fadein
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_animateContent.js
+++ app/assets/js/components/_component_animateContent.js
@@ -6,14 +6,16 @@ Hiof.FadeInContent = function() {
     var toTop = Hiof.Options.distanceToTop;
     $('.fade-in').each(function(i) {
 
-        var bottom_of_object = $(this).position().top + ($(this).outerHeight() / 2);
-        var bottom_of_window = toTop + $(window).height();
+        var bottom_of_object = $(this).position().top + ($(this).outerHeight() / 2),
+            bottom_of_window = toTop + $(window).height(),
+            breakbpoint = $(this).position().top + 200;
 
 
-        //console.log(bottom_of_object);
+
+        //console.log(breakbpoint);
 
         /* If the object is completely visible in the window, fade it it */
-        if (bottom_of_window > bottom_of_object) {
+        if (bottom_of_window > bottom_of_object || bottom_of_window > breakbpoint) {
 
             $(this).animate({
                 'opacity': '1'
