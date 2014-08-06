dac26e6c9e4a0202e47e138078ce073f497b41c0
Kenneth Dahlstrøm <kenneth@keda.no>
2014-08-06 09:57:29 +0200

    - Fixed the filter
    - Removed tons of old code.
    - Added filtering functionality
    
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_study.js
+++ app/assets/js/components/_component_study.js
@@ -4,55 +4,22 @@ var Hiof = Hiof || {};
 
 Hiof.Study = {};
 
-
-$(document).on('change', '#studie input', function(e) {
-  var thisElement = $(this),
-      thisElementName = thisElement.attr("name");
-
-  console.log("Someting changed -> " + thisElementName);
-
-  //$("table.demo tbody tr:not(.footable-filtered) input[type=checkbox]").prop('checked', this.checked);
-  //$('#main table').trigger('footable_filter', {filter: thisElementName});
-
-  //alert($("input:checkbox:checked").length);
-  var searchTerm = Hiof.SetupStudyFilter();
-  var something = "<pre><code>" + searchTerm +  "</code></pre>";
-  $("#content").append(something);
-
-  //console.log(myArray);
-  $('#main table').trigger('footable_filter', {filter: searchTerm});
-});
-
-    //var fancyFilter = $("input[type=checkbox]").attr("name");
-    //console.log(fancyFilter);
-
-
-
-Hiof.SetupStudyFilter = function(e){
-  var thisFormValues = $("#studie").serializeArray(),
-      searchTerm = [];
-      //searchTerm = "";
-
-  //console.log(thisFormValues);
-  jQuery.each( thisFormValues, function( i, field ) {
-    //console.log("singe search term: " + field.name);
-    //searchTerm += field.name + " ";
-
-    searchTerm.push(field.name);
-
-    //$( "#results" ).append( field.value + " " );
-
-  });
-  //console.log("Full search term: " + searchTerm);
-  //console.log(thisForm);
-  return searchTerm;
-  //return thisFormValues;
-};
-
-//Hiof.ExecuteFilterFromUrl = function(e){
-//  e.preventDefault();
+//Hiof.Study.FilterResults = function(e){
+//
+//  //.dropdown-menu
 //};
-
+$(document).on('click', '#studie .dropdown-menu a', function(e) {
+  e.preventDefault();
+  var filter = $(this).data("filter"),
+      filterText = $(this).text();
+  $('#main table').trigger('footable_filter', {filter: filter});
+  //if(){
+  //
+  //}else{
+  //
+  //}
+  //$('#content table caption .label').after('<span style="margin-left: 10px;">Filter: <span class="label label-info">' + filterText + '</span></span>');
+});
 
 
 Hiof.Study.DuplicateStudyCourseFacts = function(){
