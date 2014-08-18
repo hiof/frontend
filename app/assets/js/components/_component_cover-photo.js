16d4764e39ffa2000665993444db906087645fc7
Kenneth Dahlstrøm <kenneth@keda.no>
2014-08-18 10:15:39 +0200

    Rewrote window.width check
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_cover-photo.js
+++ app/assets/js/components/_component_cover-photo.js
@@ -16,48 +16,51 @@ Hiof.CoverPhoto.AddCoverPhotoToPage = function (){
   }
 
 
-  if(pageType != "index"){
-    //console.log("Page is not index");
 
+  // Load the data
+  $.getJSON("/assets/js/data/cover-photo.json", function(data) {
+      // Get data from a random entry based on the pageType
+      //console.log(data.cover[pageType]);
 
-    // Load the data
-    Hiof.CoverPhoto.GetImageData(pageType);
+      var entriesInCategory = data.cover[pageType],
+          totalEntries = Object.keys(entriesInCategory).length,
+          randomEntry = entriesInCategory[Math.floor(Math.random()*totalEntries)];
+      
+      // Set the entry to be the randomEntry data
+      //entry = randomEntry;
 
+      // Callback to generate the content
+      Hiof.CoverPhoto.GenerateMarkup(randomEntry);
 
-  }else if((pageType === "index") && (Hiof.Options.windowWidth >= Hiof.Options.navigationBreakpoint)){
-
-    //console.log("Page is index, and view is larger than 770px width");
-    Hiof.CoverPhoto.GetImageData(pageType);
-  }else{
-
-  }
+  });
 
 };
 
-Hiof.CoverPhoto.GetImageData = function(pageType){
-
-    $.getJSON("/assets/js/data/cover-photo.json", function(data) {
-        // Get data from a random entry based on the pageType
-        //console.log(data.cover[pageType]);
-
-        var entriesInCategory = data.cover[pageType],
-            totalEntries = Object.keys(entriesInCategory).length,
-            randomEntry = entriesInCategory[Math.floor(Math.random()*totalEntries)];
-        
-        // Set the entry to be the randomEntry data
-        //entry = randomEntry;
-
-        // Callback to generate the content
-        Hiof.CoverPhoto.GenerateMarkup(randomEntry);
-    });
-};
+//Hiof.CoverPhoto.GetImageData = function(pageType){
+//
+//    $.getJSON("/assets/js/data/cover-photo.json", function(data) {
+//        // Get data from a random entry based on the pageType
+//        //console.log(data.cover[pageType]);
+//
+//        var entriesInCategory = data.cover[pageType],
+//            totalEntries = Object.keys(entriesInCategory).length,
+//            randomEntry = entriesInCategory[Math.floor(Math.random()*totalEntries)];
+//        
+//        // Set the entry to be the randomEntry data
+//        //entry = randomEntry;
+//
+//        // Callback to generate the content
+//        Hiof.CoverPhoto.GenerateMarkup(randomEntry);
+//    });
+//};
 
 
 Hiof.CoverPhoto.GenerateMarkup = function(data){
 
   var coverWrapper = document.createElement('div'),
         photoWrapper = $(coverWrapper).clone(),
-        blurWrapper = $(coverWrapper).clone();
+        blurWrapper = $(coverWrapper).clone(),
+        windowWidth = $(window).width();
 
 
 
@@ -71,41 +74,41 @@ Hiof.CoverPhoto.GenerateMarkup = function(data){
   //  }
   //});
 
-  if(Hiof.Options.windowWidth < 400){
+  if(windowWidth < 400){
       $(coverWrapper).addClass('height-200');
       $(photoWrapper).css('background-image', 'url(' + data[400].normal + ')').addClass('height-200');
       $(blurWrapper).css('background-image', 'url(' + data[400].blurred + ')').addClass('height-200');
 
-  }else if ((Hiof.Options.windowWidth > 400) && (Hiof.Options.windowWidth < 500)){
+  }else if ((windowWidth > 400) && (windowWidth < 500)){
       $(coverWrapper).addClass('height-200');
       $(photoWrapper).css('background-image', 'url(' + data[500].normal + ')').addClass('height-200');
       $(blurWrapper).css('background-image', 'url(' + data[500].blurred + ')').addClass('height-200');
-  }else if ((Hiof.Options.windowWidth > 500) && (Hiof.Options.windowWidth < 600)){
+  }else if ((windowWidth > 500) && (windowWidth < 600)){
       $(coverWrapper).addClass('height-300');
       $(photoWrapper).css('background-image', 'url(' + data[600].normal + ')').addClass('height-300');
       $(blurWrapper).css('background-image', 'url(' + data[600].blurred + ')').addClass('height-300');
    
-  }else if ((Hiof.Options.windowWidth > 600) && (Hiof.Options.windowWidth < 800)){
+  }else if ((windowWidth > 600) && (windowWidth < 800)){
       $(coverWrapper).addClass('height-300');
       $(photoWrapper).css('background-image', 'url(' + data[800].normal + ')').addClass('height-300');
       $(blurWrapper).css('background-image', 'url(' + data[800].blurred + ')').addClass('height-300');
      
-  }else if ((Hiof.Options.windowWidth > 800) && (Hiof.Options.windowWidth < 1000)){
+  }else if ((windowWidth > 800) && (windowWidth < 1000)){
       $(coverWrapper).addClass('height-600');
       $(photoWrapper).css('background-image', 'url(' + data[1000].normal + ')').addClass('height-600');
       $(blurWrapper).css('background-image', 'url(' + data[1000].blurred + ')').addClass('height-600');
     
-  }else if ((Hiof.Options.windowWidth > 1000) && (Hiof.Options.windowWidth < 1200)){
+  }else if ((windowWidth > 1000) && (windowWidth < 1200)){
       $(coverWrapper).addClass('height-600');
       $(photoWrapper).css('background-image', 'url(' + data[1200].normal + ')').addClass('height-600');
       $(blurWrapper).css('background-image', 'url(' + data[1200].blurred + ')').addClass('height-600');
       
-  }else if ((Hiof.Options.windowWidth > 1200) && (Hiof.Options.windowWidth < 1600)){
+  }else if ((windowWidth > 1200) && (windowWidth < 1600)){
       $(coverWrapper).addClass('height-600');
       $(photoWrapper).css('background-image', 'url(' + data[1600].normal + ')').addClass('height-600');
       $(blurWrapper).css('background-image', 'url(' + data[1600].blurred + ')').addClass('height-600');
      
-  }else if (Hiof.Options.windowWidth > 1600){
+  }else if (windowWidth > 1600){
       $(coverWrapper).addClass('height-600');
       $(photoWrapper).css('background-image', 'url(' + data[2000].normal + ')').addClass('height-600');
       $(blurWrapper).css('background-image', 'url(' + data[2000].blurred + ')').addClass('height-600');
