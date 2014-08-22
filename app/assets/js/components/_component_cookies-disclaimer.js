8a71c8a066a5cd42619e669b2d2e90e4b8e5b683
Kenneth Dahlstrøm <kenneth@keda.no>
2014-08-22 13:50:16 +0200

    Coded the Cookie disclaimer
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

---
+++ app/assets/js/components/_component_cookies-disclaimer.js
@@ -0,0 +1,28 @@
+(function() {
+    var Hiof = Hiof || {};
+    Hiof.CookieDisclaimer = {};
+
+
+    Hiof.CookieDisclaimer.AddCookieDisclaimerToPage = function() {
+        var cookieDisclaimer = '<div id="cookie-disclaimer" class="cookie-disclaimer"> ' +
+            '  <p>Høgskolen i Østfold bruker cookies på sine nettsider for bl.a. statistikk og forbedring av innholdet.</p> ' +
+            '  <p>Bruk siden som normalt, eller lukk informasjonsboksen for å akseptere bruk av cookies. <a href="http://www.hiof.no/bruk-av-cookies" target="_blank" title="Bruk av cookies" class="local">Les mer om bruken av cookies.</a></p> ' +
+            '  <a href="#" id="btnClose"  class="btn btn-primary btn-line cookie-disclaimer-close">Lukk</a> ' +
+            '</div>';
+
+        $("#body").append($(cookieDisclaimer));
+    };
+
+    $(function() {
+        if ($.cookie('CookieAccept') != 'true') {
+            Hiof.CookieDisclaimer.AddCookieDisclaimerToPage();
+        }
+        $("#body").on('click', '.cookie-disclaimer-close', function() {
+            $.cookie('CookieAccept', 'true', {
+                path: '/',
+                expires: 60
+            });
+            document.getElementById('cookie-disclaimer').style.display = 'none';
+        });
+    });
+})();
