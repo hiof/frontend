f35fb4908d64554405ff50670746ec9d8b09fa1d
Kenneth Dahlstrøm <kenneth@keda.no>
2014-10-02 16:13:31 +0200

    Updated redirect-emplyee functionality
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- Gruntfile.js
+++ Gruntfile.js
@@ -318,11 +318,12 @@ module.exports = function(grunt) {
           'app/vendor/footable/js/footable.filter.js', 
           'app/vendor/footable/js/footable.sort.js', 
           'app/vendor/footable/js/footable.striping.js',
+          'app/vendor/bootstrap/js/modal.js',
+          'app/vendor/bootstrap/js/dropdown.js', 
           'app/vendor/jquery-cookie/jquery.cookie.js',
           'app/assets/js/components/*.js', 
           'app/assets/js/*.js', 
           'app/vendor/jquery.scrollTo/jquery.scrollTo.js', 
-          'app/vendor/bootstrap/js/dropdown.js', 
           'app/vendor/slideout/slideout-navigation.js'
         ],
         dest: 'tmp/js/application.min.js'
