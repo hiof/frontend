1e75c2981d2c5540d42c860c94c92fa0d38332f3
Kenneth Dahlstrøm <kenneth@keda.no>
2014-11-18 14:44:37 +0100

    Adjusted the toolbar
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

--- app/assets/js/components/_component_share.js
+++ app/assets/js/components/_component_share.js
@@ -1,19 +1,37 @@
-
-Hiof.Share = {};
-
-
-Hiof.Share.Facebook = function(){
-  var url = escape(document.URL),
-      shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + url;
-  //console.log(shareUrl);
-  window.open(shareUrl,'_blank');
-};
+(function(Hiof, undefined) {
 
 
+    //Hiof.Share = {};
 
-Hiof.Share.Twitter = function(){
-  var url = escape(document.URL),
-      shareUrl = "https://twitter.com/share?url=" + url;
-  //console.log(shareUrl);
-  window.open(shareUrl,'_blank');
-};
+
+    shareFacebook = function() {
+        var url = escape(document.URL),
+            shareUrl = "https://www.facebook.com/sharer/sharer.php?u=" + url;
+        //console.log(shareUrl);
+        window.open(shareUrl, '_blank');
+    };
+
+
+
+    shareTwitter = function() {
+        var url = escape(document.URL),
+            shareUrl = "https://twitter.com/share?url=" + url;
+        //console.log(shareUrl);
+        window.open(shareUrl, '_blank');
+    };
+    shareLinkedIn = function() {
+        var url = escape(document.URL),
+            title = escape($('#content header *').text()),
+            shareUrl = "https://twitter.com/shareArticle?mini=true&url=" + url + '&title=' + title;
+        window.open(shareUrl, '_blank');
+    };
+
+
+
+    // Expose functions to the window
+    window.Hiof.shareFacebook = shareFacebook;
+    window.Hiof.shareTwitter = shareTwitter;
+    window.Hiof.shareLinkedIn = shareLinkedIn;
+
+
+})(window.Hiof = window.Hiof || {});
