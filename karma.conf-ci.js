15b7548158b88c79e55ab22203bcb1d667f0900d
Kenneth Dahlstrøm <kenneth@keda.no>
2015-03-02 13:34:25 +0100

    Initial karma / Souce labs testing
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

---
+++ karma.conf-ci.js
@@ -0,0 +1,95 @@
+var fs = require('fs');
+
+module.exports = function(config) {
+
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+      process.exit(1);
+    } else {
+      process.env.SAUCE_USERNAME = require('./sauce').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+    }
+  }
+
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome'
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      browserName: 'firefox'
+    },
+    'SL_Safari': {
+      base: 'SauceLabs',
+      browserName: 'safari'
+    },
+    'SL_Opera': {
+      base: 'SauceLabs',
+      browserName: 'opera'
+    },
+    'SL_ie9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '9'
+    },
+    'SL_ie10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '10'
+    },
+    'SL_ie9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      version: '11'
+    }
+  };
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['qunit'],
+    plugins: ['karma-qunit'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'tests/qunit/**/*.html',
+      'tests/qunit/*.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots', 'saucelabs'],
+
+
+    // web server port
+    port: 9876,
+
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    sauceLabs: {
+      testName: 'Karma and Sauce Labs demo'
+    },
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers),
+    singleRun: true
+  });
+};
